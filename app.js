6bdec91b4dd74650cf0757c7811b629b0d5bcb28
c <chadreed.intl@gmail.com>
2013-07-22 17:40:37 -0700

    added datachunking

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var qs = require('querystring');
 
 var newPostFormHTML = fs.readFileSync('views/posts/new.html');
 
@@ -11,17 +12,40 @@ function renderNewPostForm(request, response) {
   response.end(newPostFormHTML);
 };
 
+function addNewPost (request, response) {
+	console.log('Received a new post');
+	response.end();
+};
+
+
+//Utils
 function render404(request, response) {
   response.writeHead(404);
   response.end('404 File Not Found');
 };
 
+function parseBody(request, response) {
+  var body = '';
+  request.on('data', function(chunk){
+  	body += chunk;
+  });
+  request.on('end', function(){
+  	callback(body)
+  });
+};
+
+//Routes
+var newPostFormRegex = new RegExp('^/posts/new/?$');
+var newPostRegex = new RegExp('^/posts/?$');
+
+//Server
 var server = http.createServer(function(request, response){
-  var newPostFormRegex = new RegExp('^/posts/new/?$');
-  var pathName = url.parse(request.url).pathname;
-  if (newPostFormRegex.test(pathName)) {
+  var pathname = url.parse(request.url).pathname;
+  if (newPostFormRegex.test(pathname)) {
   	renderNewPostForm(request, response);
-  } else {
+  }else if (newPostRegex.test(pathname)) {
+    addNewPost(request, response);
+  }else {
   	render404(request, response);
   }
 });
